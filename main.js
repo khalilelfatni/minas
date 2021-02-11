a4bff07af6eed2452409b4686b92c7e5f5121ec1
khalil012 <elfatnikhalil@gmail.com>
2021-02-12 00:31:52 +0100

    .

--- main.js
+++ main.js
@@ -44,10 +44,10 @@ function difficult(){
 function lookBomb(){
     $("#box00").html('1');
     $("#box01").html('2');
-    $("#box10").html('3');
+    $("#box10").html('<i class="fas fa-bomb"></i>');
+    $("#box10").css({"background-color":"red" , "box-shadow": "0px 1px 2px 3px red" , "display":"flex" , "justify-content":"center"});
     $("#box00").css({"background-color":"#707b7c" , "display":"flex" , "justify-content":"center"});
     $("#box01").css({"background-color":"#707b7c" , "display":"flex" , "justify-content":"center"});
-    $("#box10").css({"background-color":"#707b7c" , "display":"flex" , "justify-content":"center"});
     $("#box11").css({"background-color":"#707b7c" , "display":"flex" , "justify-content":"center"});
     $("#box05").css({"background-color":"#707b7c", "display":"flex" , "justify-content":"center"});
 }
